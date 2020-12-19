06c86438abaaf87f7df9dd46a7057061ef465a76
Oleh <orenkole@gmail.com>
2020-12-19 19:35:59 +0200

    107. Converting Date Strings to Dates

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -9,12 +9,12 @@ enum MatchResult {
   Draw = "D"
 }
 
-let manUntedWins = 0;
+let manUnitedWins = 0;
 for(let match of reader.data) {
   if(match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUntedWins++;
+    manUnitedWins++;
   } else if(match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUntedWins++;
+    manUnitedWins++;
   }
 }
-console.log(manUntedWins);
+console.log(manUnitedWins);
