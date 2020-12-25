6c38c691a956799dce52aa5bb63aa4a25a28c7b5
Oleh <orenkole@gmail.com>
2020-12-25 14:03:49 +0200

    127. Building the Reporter. Plus buildAndPrintReport in Summary

--- stats/src/Summary.ts
+++ stats/src/Summary.ts
@@ -11,5 +11,11 @@ export interface OutputTarget {
 export class Summary {
   constructor(
     public analyzer: Analyzer,
-    public outputTarget: OutputTarget) {}
+    public outputTarget: OutputTarget
+  ) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
