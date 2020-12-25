ae006cf599864e3449f5443c3884a0320b350857
Oleh <orenkole@gmail.com>
2020-12-25 13:20:06 +0200

    124. Goal Moving Forward. Before implementint composition for Summary

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -11,15 +11,23 @@ interface DataReader {
 export class MatchReader {
   constructor(public reader: DataReader) {}
 
-  // .map((row: string[]): MatchData => {
-  //   return [
-  //     dateStringToDate(row[0]),
-  //     row[1],
-  //     row[2],
-  //     parseInt(row[3]),
-  //     parseInt(row[4]),
-  //     row[5] as MatchResult,
-  //     row[6]
-  //   ]
-  // })
+  matches: MatchData[] = [];
+
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data
+    .map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult,
+        row[6]
+      ]
+    })
+  }
+
+
 }
